a0f2d5bd82a5bc76aefdb540f9093d7fc51e12f8
Ribs <ribs351@gmail.com>
2026-05-05 07:00:18 +0700

    First update in a few years lmao xd

--- stats/effects/outback_crikey/crikey.lua
+++ stats/effects/outback_crikey/crikey.lua
@@ -1,10 +1,22 @@
 function init()
 	max = effect.getParameter("maxPowerMultiplier", 3)
+	min = effect.getParameter("minPowerMultiplier", 0.75)
+	maxSpeed = effect.getParameter("maxSpeedModifier", 1.75)
+	minSpeed = effect.getParameter("minSpeedModifier", 0.85)
 	groupId = effect.addStatModifierGroup({})
+
+	baseRunSpeed = 14
+	baseWalkSpeed = 8
 end
 
 function update(dt)
 	local health = status.resourcePercentage("health")
-	local mul = max + (1 - max) * health
-  effect.setStatModifierGroup(groupId, {{stat = "powerMultiplier", effectiveMultiplier = mul}})
+	local powerMul = max + (min - max) * health
+	local speedMul = maxSpeed + (minSpeed - maxSpeed) * health
+
+	effect.setStatModifierGroup(groupId, {{stat = "powerMultiplier", effectiveMultiplier = powerMul}})
+
+	mcontroller.controlParameters({
+		runSpeed = baseRunSpeed * speedMul
+	})
 end
